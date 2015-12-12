c2f6f4c02088199dcc5f3d4d6e730afb084f8115
Iain Steers <iainsteers@gmail.com>
2015-12-12 22:58:35 +0100

    Default params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,13 @@
+# Class: htcondor_ce::params
 class htcondor_ce::params {
 
+  $condor_view_hosts = ['collector1.opensciencegrid.org:9619','collector2.opensciencegrid.org:9619']
+  $ce_version        = '1.20-1.el6'
+  $lrms              = 'condor'
+  $lrms_version      = '8.3.8-1.el6'
+  $uid_domain        = $::domain
+  $gsi_regex         = '^\/DC\=ch\/DC\=cern\/OU\=computers\/CN\=(host\/)?([A-Za-z0-9.\-]*)$'
+  $gsi_backend       = 'argus'
+  $use_static_shadow = true
+  $manage_service    = true
 }
